4a60420c67a9e6dd9693ee40c62f9df93875c27c
Skall34 <pascal.buirey@gmail.com>
2025-01-18 11:44:23 +0100

    NEW PLUGIN : Chartfox !
    En fait, c'est un simple navigateur web, embarqué, qui par defaut vient se connecter au site chartfox. On peut dupliquer ce plugin pour se connecter à d'autres sites, il suffit de modifier le fichier setting.json

---
+++ plugins/ChartFoxPlugin/WebBrowserCtrl.cs
@@ -0,0 +1,105 @@
+﻿using SimAddonLogger;
+using SimAddonPlugin;
+using SimDataManager;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace ChartFoxPlugin
+{
+    public partial class WebBrowserCtrl : UserControl, ISimAddonPluginCtrl
+    {
+        public class JsonData
+        {
+            public string Name { get; set; }
+            public string Url { get; set; }
+        }
+
+        JsonData settings;
+
+        public WebBrowserCtrl()
+        {
+            loadSettings();
+            InitializeComponent();
+
+            InitializeWebView2();
+        }
+
+        void loadSettings()
+        {
+            string currentDir = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            
+            string filePath = Path.Combine(Path.GetDirectoryName(currentDir),"settings.json"); // Path to your JSON file
+            try
+            {
+                // Read the JSON file content
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Deserialize the JSON content into a C# object
+                settings = JsonSerializer.Deserialize<JsonData>(jsonContent);
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteLine($"An error occurred: {ex.Message}");
+                settings = new JsonData();
+                settings.Name = "google";
+                settings.Url = "https://www.google.com";
+                string jsonContent = JsonSerializer.Serialize(settings, new JsonSerializerOptions { WriteIndented = true });
+                try
+                {
+                    // Write the JSON content to the file
+                    File.WriteAllText(filePath, jsonContent);
+
+                    Logger.WriteLine($"JSON file successfully written to {filePath}");
+                }
+                catch (Exception ex2)
+                {
+                    Logger.WriteLine($"An error occurred: {ex2.Message}");
+                }
+            }
+        }
+
+        public event ISimAddonPluginCtrl.UpdateStatusHandler OnStatusUpdate;
+        public event ISimAddonPluginCtrl.OnTalkHandler OnTalk;
+        public event ISimAddonPluginCtrl.OnSimEventHandler OnSimEvent;
+        public event ISimAddonPluginCtrl.OnShowMsgboxHandler OnShowMsgbox;
+
+        public void FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        public string getName()
+        {
+            return settings.Name;
+        }
+
+        public void init(ref simData _data)
+        {
+        }
+
+        public void registerPage(TabControl parent)
+        {
+            parent.SuspendLayout();
+            TabPage pluginPage = new TabPage();
+            pluginPage.Text = getName();
+            pluginPage.Controls.Add(this);
+            this.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            this.Dock = DockStyle.Fill;
+            pluginPage.Visible = true;
+            parent.TabPages.Add(pluginPage);
+            parent.ResumeLayout();
+        }
+
+        public void SetWindowMode(ISimAddonPluginCtrl.WindowMode mode)
+        {
+        }
+
+        public void updateSituation(situation data)
+        {
+        }
+        private async void InitializeWebView2()
+        {
+            await webView21.EnsureCoreWebView2Async(null); // Initializes the control
+            webView21.Source = new Uri(settings.Url);
+        }
+    }
+}
